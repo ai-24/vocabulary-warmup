1e949b2aae465f978591ee0f197695e799344869
ai-24 <ai.morikawa2410sj@gmail.com>
2023-02-01 16:06:54 +0900

    Add security settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
 
   def current_user
