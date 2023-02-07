9bf140f09b51aed98456dbde0e331aeb3fe016df
ai-24 <ai.morikawa2410sj@gmail.com>
2023-02-07 13:03:40 +0900

    Organise tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,6 @@ module.exports = {
     './app/views/**/*.html.slim',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.vue'
   ]
 }
