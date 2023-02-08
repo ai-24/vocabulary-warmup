def478fffc43ca93b0766dca78bb805353bf02dd
ai-24 <ai.morikawa2410sj@gmail.com>
2023-02-08 15:08:13 +0900

    Show user answers list

--- app/javascript/components/word-quiz.vue
+++ app/javascript/components/word-quiz.vue
@@ -1,55 +1,67 @@
 <template>
   <div class="grid justify-items-stretch place-content-center">
     <div class="w-96">
-      <p>{{ $t('quiz.question') }}</p>
-      <template
-        v-for="(quizResource, index) in quizResources"
-        :key="quizResource.id">
-        <p v-if="currentIndex === index">
-          {{ quizResource.explanation }}
+      <WordQuizResult
+        v-if="isResult"
+        :userAnswers="userAnswersList"></WordQuizResult>
+      <div v-else>
+        <p>{{ $t('quiz.question') }}</p>
+        <template
+          v-for="(quizResource, index) in quizResources"
+          :key="quizResource.id">
+          <p v-if="currentIndex === index">
+            {{ quizResource.explanation }}
+          </p>
+        </template>
+        <input
+          v-if="!answered"
+          v-model="userAnswer"
+          :placeholder="$t('quiz.placeholder')" />
+        <p v-else>{{ this.userAnswer }}</p>
+        <p v-if="answered && isCorrect">◯ {{ $t('quiz.correct') }}!</p>
+        <p v-else-if="answered && !this.userAnswer">
+          ×{{ $t('quiz.answer', { answer: this.correctAnswer }) }}
         </p>
-      </template>
-      <input
-        v-if="!answered"
-        v-model="userAnswer"
-        :placeholder="$t('quiz.placeholder')" />
-      <p v-else>{{ this.userAnswer }}</p>
-      <p v-if="answered && isCorrect">◯ {{ $t('quiz.correct') }}!</p>
-      <p v-else-if="answered && !this.userAnswer">
-        ×{{ $t('quiz.answer', { answer: this.correctAnswer }) }}
-      </p>
-      <p v-else-if="answered && !isCorrect">
-        ×{{ $t('quiz.incorrect') }}
-        {{ $t('quiz.answer', { answer: this.correctAnswer }) }}
-      </p>
-      <div>
-        <div v-if="isLastQuestion && answered">
-          <p>{{ $t('quiz.completed') }}</p>
-          <button>
-            {{ $t('quiz.result') }}
+        <p v-else-if="answered && !isCorrect">
+          ×{{ $t('quiz.incorrect') }}
+          {{ $t('quiz.answer', { answer: this.correctAnswer }) }}
+        </p>
+        <div>
+          <div v-if="isLastQuestion && answered">
+            <p>{{ $t('quiz.completed') }}</p>
+            <button @click="getResult">
+              {{ $t('quiz.resultButton') }}
+            </button>
+          </div>
+          <button v-else-if="answered" @click="getNextQuestion">
+            {{ $t('quiz.next') }}
           </button>
+          <button v-else @click="onSubmit">{{ $t('quiz.submit') }}</button>
         </div>
-        <button v-else-if="answered" @click="getNextQuestion">
-          {{ $t('quiz.next') }}
-        </button>
-        <button v-else @click="onSubmit">{{ $t('quiz.submit') }}</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import WordQuizResult from './word-quiz-result.vue'
+
 export default {
   name: 'WordQuiz',
+  components: {
+    WordQuizResult
+  },
   data() {
     return {
       userAnswer: '',
+      userAnswersList: [],
       quizResources: [],
       currentIndex: 0,
       correctAnswer: '',
       answered: false,
       isCorrect: '',
-      isLastQuestion: false
+      isLastQuestion: false,
+      isResult: false
     }
   },
   methods: {
@@ -57,6 +69,7 @@ export default {
       this.getCorrectAnswer()
       this.answered = true
       this.isCorrect = this.checkUserAnswer()
+      this.createUserAnswersList()
     },
     getNextQuestion() {
       this.answered = false
@@ -64,6 +77,9 @@ export default {
       this.checkQuestionIndex()
       this.userAnswer = ''
       this.correctAnswer = ''
+    },
+    getResult() {
+      this.isResult = true
     },
     async fetchQuizResources() {
       const quizResources = await fetch('/api/quiz', {
@@ -89,6 +105,13 @@ export default {
       const lastIndex = this.quizResources.length - 1
       if (this.currentIndex !== lastIndex) return
       this.isLastQuestion = true
+    },
+    createUserAnswersList() {
+      if (this.isCorrect) {
+        this.userAnswersList.push(`◯ ${this.userAnswer}`)
+      } else {
+        this.userAnswersList.push(`× ${this.userAnswer}`)
+      }
     }
   },
   mounted() {
