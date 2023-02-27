dece2458ccec9669984d44698b8b5996b1b44f35
ai-24 <ai.morikawa2410sj@gmail.com>
2023-02-28 01:07:13 +0900

    Create tags and taggings table

--- app/models/expression.rb
+++ app/models/expression.rb
@@ -2,6 +2,8 @@
 
 class Expression < ApplicationRecord
   has_many :expression_items, dependent: :destroy
+  has_many :taggings, dependent: :destroy
+  has_many :tags, through: :taggings
   accepts_nested_attributes_for :expression_items, reject_if: lambda { |attributes|
     attributes['content'].blank?
   }
