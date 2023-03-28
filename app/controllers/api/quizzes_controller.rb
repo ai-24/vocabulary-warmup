eae351e1f685a710064d1868a26687f7b8528329
ai-24 <ai.morikawa2410sj@gmail.com>
2023-03-28 20:40:32 +0900

    Delete a method that is for xhr because fetch is used

--- app/controllers/api/quizzes_controller.rb
+++ app/controllers/api/quizzes_controller.rb
@@ -1,17 +1,7 @@
 # frozen_string_literal: true
 
 class Api::QuizzesController < ApplicationController
-  before_action :check_xhr_header
-
   def show
     @quiz = ExpressionItem.all
-  end
-
-  private
-
-  def check_xhr_header
-    return if request.xhr?
-
-    render json: { error: 'forbidden' }, status: :forbidden
   end
 end
