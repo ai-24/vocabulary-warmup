37769cf3e80321103d406cf42486c364ebefe971
ai-24 <ai.morikawa2410sj@gmail.com>
2023-04-07 15:28:43 +0900

    Create spec system of deleting expression

--- spec/system/expressions/expression_details_spec.rb
+++ spec/system/expressions/expression_details_spec.rb
@@ -138,4 +138,51 @@ RSpec.describe 'Expressions' do
       end
     end
   end
+
+  describe 'delete expressions' do
+    before do
+      FactoryBot.create_list(:expression_item, 2, expression: FactoryBot.create(:note))
+      visit '/'
+      click_link 'balcony and Veranda'
+    end
+
+    it 'check if expression is deleted' do
+      expect do
+        click_button '削除'
+        expect(page.accept_confirm).to eq 'この英単語又はフレーズを本当に削除しますか？'
+        expect(page).to have_content '英単語又はフレーズを削除しました'
+      end.to change(Expression, :count).by(-1).and change(ExpressionItem, :count).by(-2)
+
+      visit '/'
+      expect(page).not_to have_link 'balcony and Veranda'
+    end
+
+    it 'check if the expression is not deleted when cancel button is clicked' do
+      expect do
+        click_button '削除'
+        expect(page.dismiss_confirm).to eq 'この英単語又はフレーズを本当に削除しますか？'
+        within '.title' do
+          expect(page).to have_content 'balcony'
+        end
+        click_link '英単語・フレーズ一覧に戻る'
+      end.to change(Expression, :count).by(0).and change(ExpressionItem, :count).by(0)
+
+      expect(page).to have_link 'balcony and Veranda'
+    end
+
+    it 'check if the next expression is on the page after deleting expression' do
+      delete_expression_item = ExpressionItem.find_by content: 'balcony'
+      next_expression = delete_expression_item.expression.next
+      accept_confirm do
+        click_button '削除'
+      end
+      expect(page).to have_content '英単語又はフレーズを削除しました'
+
+      next_expression.expression_items.each do |expression_item|
+        within '.title' do
+          expect(page).to have_content expression_item.content
+        end
+      end
+    end
+  end
 end
