54b27ea57e42cc97ddbdc40be238c2848407d1ab
ai-24 <ai.morikawa2410sj@gmail.com>
2023-04-29 01:00:23 +0900

    Make create_all! method in Bookmarking Module because it is needed by Memorising as well

--- app/controllers/api/bookmarkings_controller.rb
+++ app/controllers/api/bookmarkings_controller.rb
@@ -3,7 +3,7 @@
 class Api::BookmarkingsController < ApplicationController
   def create
     if logged_in?
-      Bookmarking.create_all!(current_user.id, bookmarking_params[:expression_id])
+      Bookmarking.create_bookmarkings_or_memorisings!(current_user.id, bookmarking_params[:expression_id])
       head :no_content
     else
       head :unauthorized
--- spec/models/bookmarking_spec.rb
+++ spec/models/bookmarking_spec.rb
@@ -3,20 +3,5 @@
 require 'rails_helper'
 
 RSpec.describe Bookmarking, type: :model do
-  let!(:user) { FactoryBot.create(:user) }
-  let!(:first_expression_items) { FactoryBot.create_list(:expression_item, 2, expression: FactoryBot.create(:empty_note)) }
-  let!(:second_expression_items) { FactoryBot.create_list(:expression_item, 2, expression: FactoryBot.create(:empty_note)) }
-  let!(:bookmarking) { FactoryBot.create(:bookmarking) }
-
-  describe '.create_all!' do
-    it 'create bookmarkings' do
-      expression_ids = [first_expression_items[0].expression_id, second_expression_items[0].expression_id]
-      expect { described_class.create_all!(user.id, expression_ids) }.to change(described_class, :count).by(2)
-    end
-
-    it 'create one bookmarking but not the one that already exists' do
-      expression_ids = [bookmarking.expression_id, first_expression_items[0].expression_id]
-      expect { described_class.create_all!(bookmarking.user_id, expression_ids) }.to change(described_class, :count).by(1)
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
