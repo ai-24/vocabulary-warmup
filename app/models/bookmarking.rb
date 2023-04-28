65fda7b52f93de715e52075706d5034ecb7e5159
ai-24 <ai.morikawa2410sj@gmail.com>
2023-04-29 01:27:50 +0900

    Add Validation to Bookmarking

--- app/models/bookmarking.rb
+++ app/models/bookmarking.rb
@@ -5,4 +5,6 @@ class Bookmarking < ApplicationRecord
 
   belongs_to :user
   belongs_to :expression
+
+  validates :user_id, uniqueness: { scope: :expression_id }
 end
--- spec/models/bookmarking_spec.rb
+++ spec/models/bookmarking_spec.rb
@@ -3,5 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe Bookmarking, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'bookmarking cannot be saved if same record exists' do
+      user = FactoryBot.create(:user)
+      expression_items = FactoryBot.create_list(:expression_item, 2, expression: FactoryBot.create(:empty_note))
+      new_memorising = described_class.new(user_id: user.id, expression_id: expression_items[0].expression.id)
+      expect(new_memorising).to be_valid
+
+      bookmarking = FactoryBot.create(:bookmarking)
+      same_bookmarking = described_class.new(user_id: bookmarking.user_id, expression_id: bookmarking.expression_id)
+      expect(same_bookmarking).not_to be_valid
+    end
+  end
 end
