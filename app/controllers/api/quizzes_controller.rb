fc54402f32394ed67974f1932f07af7247acecfd
ai-24 <ai.morikawa2410sj@gmail.com>
2023-05-09 12:11:49 +0900

    Chanege questions for quiz because the expressions in main list are changed

--- app/controllers/api/quizzes_controller.rb
+++ app/controllers/api/quizzes_controller.rb
@@ -2,6 +2,16 @@
 
 class Api::QuizzesController < ApplicationController
   def show
-    @quiz = ExpressionItem.all
+    @quiz = []
+
+    if logged_in?
+      Expression.find_expressions_of_users_main_list(current_user.id).each do |expression|
+        expression.expression_items.each { |expression_item| @quiz.push expression_item }
+      end
+    else
+      Expression.where(user_id: nil).each do |expression|
+        expression.expression_items.each { |expression_item| @quiz.push expression_item }
+      end
+    end
   end
 end
